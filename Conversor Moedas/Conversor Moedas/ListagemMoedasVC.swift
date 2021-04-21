0050e8dd517116d897b4f8fd71acb4fde5c5284b
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-04-21 18:09:54 -0300

    Implementado o layout Listagem de Moedas e as ligações.

---
+++ Conversor Moedas/Conversor Moedas/ListagemMoedasVC.swift
@@ -0,0 +1,21 @@
+//
+//  ListagemMoedasVC.swift
+//  Conversor Moedas
+//
+//  Created by Alexandre Cardoso on 21/04/21.
+//
+
+import UIKit
+
+class ListagemMoedasVC: UIViewController {
+	
+	// MARK: - IBOutlet
+	@IBOutlet weak var tableView: UITableView!
+	
+	
+	// MARK: - Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+		
+}
