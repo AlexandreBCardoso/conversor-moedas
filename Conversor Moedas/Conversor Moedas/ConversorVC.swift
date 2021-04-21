16724016d261cca9a7c47befe361d1ce6dbb9a47
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-04-21 17:56:37 -0300

    implementado layout da tela conversão e ligações de elementos UI

---
+++ Conversor Moedas/Conversor Moedas/ConversorVC.swift
@@ -0,0 +1,33 @@
+//
+//  ConversorVC.swift
+//  Conversor Moedas
+//
+//  Created by Alexandre Cardoso on 21/04/21.
+//
+
+import UIKit
+
+class ConversorVC: UIViewController {
+	
+	// MARK: - IBOutlet
+	@IBOutlet weak var moedaOriLabel: UILabel!
+	@IBOutlet weak var valueTextField: UITextField!
+	@IBOutlet weak var moedaDesLabel: UILabel!
+	@IBOutlet weak var valueConvertidoLabel: UILabel!
+	
+	
+	// MARK: - Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	
+	// MARK: - IBAction
+	@IBAction func tappedTrocaMoedaButton(_ sender: UIButton) {
+	}
+	
+	@IBAction func tappedConverterButton(_ sender: UIButton) {
+	}
+	
+		
+}
